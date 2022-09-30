cd41af600c2c0a5a07fa37b215a6f8ccaa15aafa
Pedro Henrique <99281954+Mrdiskman@users.noreply.github.com>
2022-09-30 18:27:03 -0300

    s2-24 completa

--- crud.sql
+++ crud.sql
@@ -4,31 +4,36 @@
 -- Criação
 
 -- 1)
-
-
-
+INSERT INTO clientes(nome, lealdade)
+  VALUES('Georgia', 0)
 -- 2)
-
-
-
+INSERT INTO pedidos(status, cliente_id)
+  VALUES('Recebido', (SELECT id FROM clientes WHERE nome = 'Georgia'));
 -- 3)
-
-
-
+  INSERT INTO produtos_pedidos(pedido_id, produto_id)
+VALUES(6,1),(6,2),(6,6),(6,8),(6,8);
 -- Leitura
 
 -- 1)
-
+SELECT 
+ cl.id, cl.nome, cl.lealdade,
+ pe.status, pe.cliente_id, pe.id,
+ pr.id, pr.nome, pr.tipo, pr.preço, pr.pts_de_lealdade
+ 
+  FROM clientes cl
+  JOIN pedidos pe ON pe.cliente_id = cl.id
+  JOIN produtos_pedidos pr_pe ON pr_pe.pedido_id = pe.id
+  JOIN produtos pr ON pr.id = pr_pe.produto_id
+  
+  WHERE cl.nome = 'Georgia'
 
 
 -- Atualização
 
 -- 1)
-
-
-
+UPDATE clientes cl SET lealdade = 48 WHERE cl.nome = 'Georgia';
 -- Deleção
 
 -- 1)
-
-
+DELETE FROM clientes cl WHERE cl.nome = 'Marcelo'
+  RETURNING *;
--- selects.sql
+++ selects.sql
@@ -2,11 +2,40 @@
 -- Seleções de dados
 
 -- 1)
-
+SELECT 
+  pe.id, pe.status, pe.cliente_id,
+  pr.id, pr.nome, pr.tipo, pr.preço, pr.pts_de_lealdade
+FROM pedidos pe
+  JOIN produtos_pedidos pr_pe ON pe.id = pr_pe.pedido_id
+  JOIN produtos pr ON pr.id = pr_pe.produto_id
 -- 2)
-
+SELECT 
+  pe.id
+FROM pedidos pe
+  JOIN produtos_pedidos pr_pe ON pe.id = pr_pe.pedido_id
+  JOIN produtos pr ON pr.id = pr_pe.produto_id
+  WHERE pr.nome = 'Fritas';
 -- 3)
-
+SELECT 
+  cl.nome gostam_de_fritas
+FROM pedidos pe
+  JOIN produtos_pedidos pr_pe ON pe.id = pr_pe.pedido_id
+  JOIN produtos pr ON pr.id = pr_pe.produto_id
+  JOIN clientes cl ON cl.id = pe.cliente_id
+  WHERE pr.nome = 'Fritas';
 -- 4)
-
+SELECT
+ SUM(pr.preço) AS sum
+FROM produtos pr
+  JOIN produtos_pedidos pr_pe ON pr.id = pr_pe.produto_id
+  JOIN pedidos pe ON pe.id = pr_pe.pedido_id
+  JOIN clientes cl ON cl.id = pe.cliente_id
+WHERE cl.nome = 'Laura';
 -- 5)
+SELECT 
+	pr.nome nome, COUNT(pe)
+  FROM produtos pr
+  JOIN produtos_pedidos pr_pe ON pr_pe.produto_id = pr.id
+  JOIN pedidos pe ON pr_pe.pedido_id = pe.id
+  
+  GROUP BY pr.nome ORDER BY pr.nome ASC;
